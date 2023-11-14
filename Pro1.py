ff4a588f546c9000267b954db959b5b19e872edf
Your Name <zeeshanhaider612@gmail.com>
2023-11-14 16:22:48 +0500

    feat/Pro1/function that sort two arrays in place

---
+++ Pro1.py
@@ -0,0 +1,23 @@
+arr=[1,7,10,12,13]
+arr2=[6,7,9,11,14]
+
+
+def sorter(arr1,arr2):
+    ptr1=0
+    ptr2=0
+    while ptr1<len(arr1) and ptr2<len(arr2):
+        print("arr2[ptr2]: ",arr2[ptr2])
+        print("arr1[ptr1]: ",arr1[ptr1])
+        print("#############")
+        if(arr2[ptr2]<=arr1[ptr1]):
+            arr.insert(ptr1,arr2[ptr2])
+            ptr2+=1
+        else:
+            ptr1+=1
+    if(ptr1>=len(arr1)):
+        while ptr2<len(arr2):
+            arr.append(arr2[ptr2])
+            ptr2+=1
+    return arr1
+
+print(sorter(arr,arr2))
